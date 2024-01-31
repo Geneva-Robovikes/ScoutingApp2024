72cf8dfd8c895f7ebededdcdd51d1c3b24cdb5fc
Geneva-Robovikes <robovikes@geneva304.org>
2024-01-31 17:08:25 -0600

    Third version

--- app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
+++ app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
@@ -4,7 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//Java doesn't seem to like this import statement
+// import org.json.simple.JSONObject;
 public class MainActivity extends AppCompatActivity {
+    //JSONObject JSONParser = new JSONObject();
+    //autonomous
+    boolean auto_canLeaveStartingZone = false;
+    boolean auto_canShootNoteintoSpeaker = false;
+    boolean auto_canShootintoAmp = false;
+    //teleoperated values
+    int speakerScore = 0;
+    int ampScore = 0;
+    boolean onStage = false;
+    boolean scoreTrap = false;
+    boolean spotlight = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
