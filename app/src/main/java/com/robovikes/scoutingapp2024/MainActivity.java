912d0a481d6a51c7e3926c30c136173777007786
robovikes <robovikes@geneva304.org>
2024-02-03 15:14:45 -0800

    Java!!!

--- app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
+++ app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
@@ -3,10 +3,24 @@ package com.robovikes.scoutingapp2024;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONObject;
 
 //Java doesn't seem to like this import statement
 // import org.json.simple.JSONObject;
 public class MainActivity extends AppCompatActivity {
+
+    public void QRgen(){
+        System.out.println("yipee!");
+    }
+
     //JSONObject JSONParser = new JSONObject();
     //autonomous
     boolean auto_canLeaveStartingZone = false;
@@ -25,5 +39,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //declaring edit texts
+        Button generateQRCode = (Button)findViewById(R.id.generateQRCODE);
+        EditText teamName = (EditText)findViewById(R.id.editTeamName);
+        EditText speakerScore = (EditText) findViewById(R.id.editSpeakerScore);
+        EditText ampScore = (EditText)findViewById(R.id.editAmpScore);
+        CheckBox onStage = (CheckBox)findViewById(R.id.onStageCheckbox);
+        CheckBox scoreTrap = (CheckBox)findViewById(R.id.scoreTrapCheckbox);
+
+        generateQRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("yipee!");
+            }
+        });
     }
 }
