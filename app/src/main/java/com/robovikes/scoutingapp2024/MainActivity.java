dd3e09f753344290539f0c1b32ee3b787c9c88c2
robovikes <robovikes@geneva304.org>
2024-02-10 14:50:33 -0800

    QR CODE WORKING

--- app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
+++ app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
@@ -1,38 +1,32 @@
 package com.robovikes.scoutingapp2024;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import org.json.simple.JSONObject;
+import androidx.appcompat.app.AppCompatActivity;
 
-//Java doesn't seem to like this import statement
-// import org.json.simple.JSONObject;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
 public class MainActivity extends AppCompatActivity {
-
-    public void QRgen(){
-        System.out.println("yipee!");
-    }
-
-    //JSONObject JSONParser = new JSONObject();
-    //autonomous
+    //declaration statements
+    String teamName;
     boolean auto_canLeaveStartingZone = false;
     boolean auto_canShootNoteintoSpeaker = false;
     boolean auto_canShootintoAmp = false;
-    //teleoperated values
     int speakerScore = 0;
     int ampScore = 0;
     boolean onStage = false;
     boolean scoreTrap = false;
     boolean spotlight = false;
-
 
 
     @Override
@@ -41,16 +35,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //declaring edit texts
         Button generateQRCode = (Button)findViewById(R.id.generateQRCODE);
-        EditText teamName = (EditText)findViewById(R.id.editTeamName);
-        EditText speakerScore = (EditText) findViewById(R.id.editSpeakerScore);
-        EditText ampScore = (EditText)findViewById(R.id.editAmpScore);
-        CheckBox onStage = (CheckBox)findViewById(R.id.onStageCheckbox);
-        CheckBox scoreTrap = (CheckBox)findViewById(R.id.scoreTrapCheckbox);
+        EditText TeamName = (EditText)findViewById(R.id.editTeamName);
+        EditText SpeakerScore = (EditText) findViewById(R.id.editSpeakerScore);
+        EditText AmpScore = (EditText)findViewById(R.id.editAmpScore);
+        CheckBox OnStage = (CheckBox)findViewById(R.id.onStageCheckbox);
+        CheckBox ScoreTrap = (CheckBox)findViewById(R.id.scoreTrapCheckbox);
+        //auto
+        CheckBox AutoSpeaker = (CheckBox) findViewById(R.id.autospeakerCheckbox);
+        CheckBox AutoAmp = (CheckBox) findViewById(R.id.autoampCheckbox);
+        CheckBox AutoLeave = (CheckBox) findViewById(R.id.autoleaveCheckbox);
+        CheckBox Spotlight = (CheckBox) findViewById(R.id.spotlightCheckbox);
+        //qr code
+        ImageView QRCode = (ImageView) findViewById(R.id.QRCODE);
 
         generateQRCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("yipee!");
+                //on button press generate json code and show QR code as image
+                //this is a mess and im sorry
+                teamName = String.valueOf(TeamName.getText());
+                auto_canLeaveStartingZone = AutoLeave.isChecked();
+                auto_canShootNoteintoSpeaker = AutoSpeaker.isChecked();
+                auto_canShootintoAmp = AutoAmp.isChecked();
+                speakerScore = Integer.parseInt(String.valueOf(SpeakerScore.getText())); //this conversion is stupid and i don't understand it
+                ampScore = Integer.parseInt(String.valueOf(AmpScore.getText())); //why can't i just return a numbers only string as an int?
+                onStage = OnStage.isChecked();
+                scoreTrap = ScoreTrap.isChecked();
+                spotlight = Spotlight.isChecked();
+                //generate qr code
+                MultiFormatWriter mWriter = new MultiFormatWriter();
+                try {
+                    //BitMatrix class to encode entered text and set Width & Height
+                    BitMatrix mMatrix = mWriter.encode(teamName, BarcodeFormat.QR_CODE, 400,400);
+                    BarcodeEncoder mEncoder = new BarcodeEncoder();
+                    Bitmap mBitmap = mEncoder.createBitmap(mMatrix);//creating bitmap of code
+                    QRCode.setImageBitmap(mBitmap);//Setting generated QR code to imageView
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                }
             }
         });
     }
