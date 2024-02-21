6f87e817b65dfd8d1e2b271b459d55856a12d524
robovikes <robovikes@geneva304.org>
2024-02-21 13:45:25 -0800

    QR code fixed

--- app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
+++ app/src/main/java/com/robovikes/scoutingapp2024/MainActivity.java
@@ -21,7 +21,9 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class MainActivity extends AppCompatActivity {
@@ -64,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
         //declaring edit texts
         Button generateQRCode = (Button)findViewById(R.id.generateQRCODE);
         EditText TeamName = (EditText)findViewById(R.id.editTeamName);
-        EditText SpeakerScore = (EditText) findViewById(R.id.editSpeakerScore);
         CheckBox OnStage = (CheckBox)findViewById(R.id.onStageCheckbox);
         CheckBox ScoreTrap = (CheckBox)findViewById(R.id.scoreTrapCheckbox);
         //auto
@@ -110,54 +111,74 @@ public class MainActivity extends AppCompatActivity {
                 AutoAmp.setText(String.valueOf(autoAmpScore));
             }
         });
+        //speaker
+        TextView SpeakerScore = (TextView) findViewById(R.id.speakerValue);
+        Button SpeakerMinus = (Button) findViewById(R.id.speakerMinus);
+        Button SpeakerPlus = (Button) findViewById(R.id.speakerPlus);
+
+        SpeakerPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakerScore++;
+                SpeakerScore.setText(String.valueOf(speakerScore));
+            }
+        });
+        SpeakerMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speakerScore--;
+                SpeakerScore.setText(String.valueOf(speakerScore));
+            }
+        });
         generateQRCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //on button press generate json code and show QR code as image
                 //this is a mess and im sorry
-                /*
-                if (String.valueOf(SpeakerScore.getText()).equals("") || String.valueOf(AmpScore.getText()).equals("") || String.valueOf(TeamName.getText()).equals("")){
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Error");
-                    builder.setMessage("QR Code can't be generated without data");
-                    builder.setCancelable(true);
-                    AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-                }
-                else{
-                    Dictionary<String, String> textMap = new Hashtable<>();
+                ArrayList<String[]> textMap = new ArrayList<String[]>();
+                String csvFile = "";
 
-                    teamName = String.valueOf(TeamName.getText());
-                    textMap.put("Team Name,",teamName);
+                teamName = String.valueOf(TeamName.getText());
+                textMap.add(new String[]{"Team Name,", teamName});
+                textMap.add(new String[]{"Auto Speaker Score,", String.valueOf(autoSpeakerScore)});
+                textMap.add(new String[]{"Auto Amp Score,", String.valueOf(autoAmpScore)});
+                textMap.add(new String[]{"Speaker Score", String.valueOf(speakerScore)});
+                //speakerScore = Integer.parseInt(String.valueOf(SpeakerScore.getText())); //this conversion is stupid and i don't understand it
+                onStage = OnStage.isChecked();
+                scoreTrap = ScoreTrap.isChecked();
+                spotlight = Spotlight.isChecked();
+                QRCode.setVisibility(View.VISIBLE);
+                //turn arraylist to string
 
-                    auto_canShootintoAmp = AutoAmp.isChecked();
-                    speakerScore = Integer.parseInt(String.valueOf(SpeakerScore.getText())); //this conversion is stupid and i don't understand it
-                    ampScore = Integer.parseInt(String.valueOf(AmpScore.getText())); //why can't i just return a numbers only string as an int?
-                    onStage = OnStage.isChecked();
-                    scoreTrap = ScoreTrap.isChecked();
-                    spotlight = Spotlight.isChecked();
-                    QRCode.setVisibility(View.VISIBLE);
-                    //generate qr code
-                    String csvFile = "Team Name," + teamName +
-                            "\nAuto: Can Leave Start," + auto_canLeaveStartingZone +
-                            "\nAuto: Can Shoot in Speaker," + auto_canShootNoteintoSpeaker +
-                            "\nSpeaker Score," + speakerScore +
-                            "\nAmp Score," + ampScore +
-                            "\nOn Stage," + onStage +
-                            "\nScore Trap," + scoreTrap +
-                            "\nSpotlight," + spotlight;
-                    MultiFormatWriter mWriter = new MultiFormatWriter();
-                    try {
-                        //BitMatrix class to encode entered text and set Width & Height
-                        BitMatrix mMatrix = mWriter.encode(csvFile, BarcodeFormat.QR_CODE, 300, 300);
-                        BarcodeEncoder mEncoder = new BarcodeEncoder();
-                        Bitmap mBitmap = mEncoder.createBitmap(mMatrix);//creating bitmap of code
-                        QRCode.setImageBitmap(mBitmap);//Setting generated QR code to imageView
-                    } catch (WriterException e) {
-                        e.printStackTrace();
+                for (String[] row : textMap){
+                    for (int i = 0; i < row.length; i++){
+                        csvFile+=row[i];
                     }
+                    csvFile += ",";
                 }
+
+
+                //generate qr code
+                /*
+                String csvFile = "Team Name," + teamName +
+                        "\nAuto: Can Leave Start," + auto_canLeaveStartingZone +
+                        "\nAuto: Can Shoot in Speaker," + auto_canShootNoteintoSpeaker +
+                        "\nSpeaker Score," + speakerScore +
+                        "\nAmp Score," + ampScore +
+                        "\nOn Stage," + onStage +
+                        "\nScore Trap," + scoreTrap +
+                        "\nSpotlight," + spotlight;
                  */
+                MultiFormatWriter mWriter = new MultiFormatWriter();
+                try {
+                    //BitMatrix class to encode entered text and set Width & Height
+                    BitMatrix mMatrix = mWriter.encode(csvFile, BarcodeFormat.QR_CODE, 300, 300);
+                    BarcodeEncoder mEncoder = new BarcodeEncoder();
+                    Bitmap mBitmap = mEncoder.createBitmap(mMatrix);//creating bitmap of code
+                    QRCode.setImageBitmap(mBitmap);//Setting generated QR code to imageView
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
             }
         });
         QRCode.setOnClickListener(new View.OnClickListener() {
